ec183632141298f3f1b1ae6e38abeaec43d20d7f
Jonathan Alvarez G <j@jonalvarezz.com>
2015-10-28 00:06:18 -0500

    Init

---
+++ src/scripts/main.js
@@ -0,0 +1,22 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  nebula = require('bespoke-theme-nebula'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  backdrop = require('bespoke-backdrop'),
+  hash = require('bespoke-hash');
+
+// Bespoke.js
+bespoke.from('article', [
+  nebula(),
+  keys(),
+  touch(),
+  backdrop(),
+  hash()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
